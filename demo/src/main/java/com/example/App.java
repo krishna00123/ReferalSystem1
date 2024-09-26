5db0cc78b0baa8ba0bcbfac141f9d9ec048b33d6
Krishna Bhattarai <67731789+krishna00123@users.noreply.github.com>
2024-09-26 13:25:36 +0530

    Update App.java

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -7,5 +7,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+         String insert = "Insert into user (id ,username ,email ,password ,salt ,role ,created_at ,updated_at ) values (?,?,?,?,?,?,?,?)";
+        String user = "admin";
+        String pass="admin";
+        String url="jdbc:mysql://localhost:3306/referalsystem1";
+        Connection con = null;
+        try{ 
+        Class.forName("com.mysql.jdbc.cj.Driver");
+        con = DriverManager.getConnection(url,user,pass);
+        System.out.println("Connection Successfull");
+        }catch(ClassNotFoundException c){
+            c.printStackTrace();
+            System.out.println("Class path not found");
+        }
+    }
+}
+
+         
+       
+   
+    
+
     }
 }
